810b49187b6b905e2f75daff083c344a313d154b
Sammy Njue <sammynjue10@gmail.com>
2025-04-05 03:03:19 +0300

    v1.13.37.1 - sum light

--- src/screens/main/ProfileScreen.js
+++ src/screens/main/ProfileScreen.js
@@ -601,7 +601,7 @@ const ProfileScreen = ({ navigation }) => {
             </Text>
           </TouchableOpacity>
           <Text style={styles.versionText} maxFontSizeMultiplier={0}>
-            v1.13.36
+            v1.13.37
           </Text>
           {/* <Text style={styles.versionText} maxFontSizeMultiplier={0}>
                         © 2025 ivory

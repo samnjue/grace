36e1c40b8e12a9c71f6ca4643a2ed1b1e918ae3f
Sammy Njue <sammynjue10@gmail.com>
2025-04-05 03:01:14 +0300

    v1.13.37 - Native adjustments

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AppNavigator from './src/navigation/AppNavigator';
-import store from './src/redux/store';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { Inter_200ExtraLight, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
-import { PlayfairDisplay_400Regular, PlayfairDisplay_400Regular_Italic, PlayfairDisplay_900Black } from '@expo-google-fonts/playfair-display';
-import { SourceSerif4_400Regular, SourceSerif4_400Regular_Italic, SourceSerif4_700Bold_Italic, SourceSerif4_900Black_Italic, SourceSerif4_700Bold } from '@expo-google-fonts/source-serif-4';
-import { Archivo_700Bold, Archivo_800ExtraBold, Archivo_900Black } from '@expo-google-fonts/archivo';
-import { Montserrat_700Bold, Montserrat_800ExtraBold, Montserrat_900Black } from '@expo-google-fonts/montserrat';
-import { StatusBar } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import * as NavigationBar from 'expo-navigation-bar';
-import { useSelector, useDispatch } from 'react-redux';
-import { setTheme } from './src/redux/slices/themeSlice';
+import React, { useEffect } from "react";
+import { Provider } from "react-redux";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppNavigator from "./src/navigation/AppNavigator";
+import store from "./src/redux/store";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  Inter_200ExtraLight,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+import {
+  PlayfairDisplay_400Regular,
+  PlayfairDisplay_400Regular_Italic,
+  PlayfairDisplay_900Black,
+} from "@expo-google-fonts/playfair-display";
+import {
+  SourceSerif4_400Regular,
+  SourceSerif4_400Regular_Italic,
+  SourceSerif4_700Bold_Italic,
+  SourceSerif4_900Black_Italic,
+  SourceSerif4_700Bold,
+} from "@expo-google-fonts/source-serif-4";
+import {
+  Archivo_700Bold,
+  Archivo_800ExtraBold,
+  Archivo_900Black,
+} from "@expo-google-fonts/archivo";
+import {
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+  Montserrat_900Black,
+} from "@expo-google-fonts/montserrat";
+import { StatusBar } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as NavigationBar from "expo-navigation-bar";
+import { useSelector, useDispatch } from "react-redux";
+import { setTheme } from "./src/redux/slices/themeSlice";
 
 function MainApp() {
   const [fontsLoaded] = useFonts({
@@ -39,17 +63,17 @@ function MainApp() {
 
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.theme.theme);
-  const isDarkTheme = theme.toLowerCase().includes('dark');
+  const isDarkTheme = theme.toLowerCase().includes("dark");
 
   useEffect(() => {
     const loadTheme = async () => {
       try {
-        const storedTheme = await AsyncStorage.getItem('appTheme');
+        const storedTheme = await AsyncStorage.getItem("appTheme");
         if (storedTheme) {
           dispatch(setTheme(storedTheme));
         }
       } catch (error) {
-        console.error('Error loading theme:', error);
+        console.error("Error loading theme:", error);
       }
     };
 
@@ -59,9 +83,9 @@ function MainApp() {
   useEffect(() => {
     const saveTheme = async () => {
       try {
-        await AsyncStorage.setItem('appTheme', theme);
+        await AsyncStorage.setItem("appTheme", theme);
       } catch (error) {
-        console.error('Error saving theme:', error);
+        console.error("Error saving theme:", error);
       }
     };
 
@@ -69,8 +93,8 @@ function MainApp() {
   }, [theme]);
 
   useEffect(() => {
-    NavigationBar.setBackgroundColorAsync(isDarkTheme ? '#121212' : '#fff');
-    NavigationBar.setButtonStyleAsync(isDarkTheme ? 'dark' : 'light');
+    NavigationBar.setBackgroundColorAsync(isDarkTheme ? "#121212" : "#fff");
+    NavigationBar.setButtonStyleAsync(isDarkTheme ? "dark" : "light");
   }, [isDarkTheme]);
 
   useEffect(() => {
@@ -94,8 +118,8 @@ function MainApp() {
   return (
     <SafeAreaProvider>
       <StatusBar
-        barStyle={isDarkTheme ? 'light-content' : 'dark-content'}
-        backgroundColor={isDarkTheme ? '#121212' : '#fff'}
+        barStyle={isDarkTheme ? "light-content" : "dark-content"}
+        backgroundColor={isDarkTheme ? "#121212" : "#fff"}
         animated
       />
       <AppNavigator />
--- src/redux/slices/themeSlice.js
+++ src/redux/slices/themeSlice.js
@@ -1,28 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { configureNavigationBar } from '../../utils/themeUtils';
+import { createSlice } from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { configureNavigationBar } from "../../utils/themeUtils";
 
 const initialState = {
-    theme: 'Light theme',
+  theme: "Light theme",
 };
 
 const themeSlice = createSlice({
-    name: 'theme',
-    initialState,
-    reducers: {
-        setTheme: (state, action) => {
-            state.theme = action.payload;
-            AsyncStorage.setItem('theme', action.payload);
-            configureNavigationBar(action.payload);
-        },
-        loadTheme: (state, action) => {
-            state.theme = action.payload || 'Light theme';
-            configureNavigationBar(action.payload || 'Light Theme');
-        },
+  name: "theme",
+  initialState,
+  reducers: {
+    setTheme: (state, action) => {
+      state.theme = action.payload;
+      AsyncStorage.setItem("theme", action.payload);
+      configureNavigationBar(action.payload);
     },
+    loadTheme: (state, action) => {
+      state.theme = action.payload || "Light theme";
+      configureNavigationBar(action.payload || "Light Theme");
+    },
+  },
 });
 
 export const { setTheme, loadTheme } = themeSlice.actions;
 export default themeSlice.reducer;
-
-
